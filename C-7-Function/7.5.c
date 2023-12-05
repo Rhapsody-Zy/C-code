0b93ced27754ab1c10001831b95dd6b30be35323
Rhapsody-Zy <151840465+Rhapsody-Zy@users.noreply.github.com>
2023-12-05 22:56:11 +0800

    Update 7.5.c

--- C-7-Function/7.5.c
+++ C-7-Function/7.5.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 
-void Swap(int x, int y)     //void意思是这个函数没有返回值，void意思为空
+void Swap1(int x, int y)     //void意思是这个函数没有返回值，void意思为空
 { 
     int tmp = 0;
     tmp = x;
     x = y;
     y = tmp;
 }
+//-------以上操作不能将两个整数类型进行互换--------
+//当实参传给形参的时候，形参其实是实参的一份临时拷贝，对形参的修改是不会改变实参的
+
+void Swap2(int* pa,int* pb)     //void意思是这个函数没有返回值，void意思为空
+{ 
+    int tmp = 0;
+    tmp = *pa;
+    *pa = *pb;
+    *pb = tmp;
+}
+
 int main()
 {
     int a = 10;
     int b = 20;
     printf("a=%d b=%d\n", a, b);
-    Swap(a, b);
+    Swap2(&a, &b);
     printf("a=%d b=%d\n", a, b);
     return 0;
 }
+
+// int main()
+// {
+//     int a = 0;
+//     int* pa = &a;    //pa用于存放a的地址，pa此时为指针变量，用int*进行修饰
+//     *pa = 20;        //解引用操作
+//     return 0;
+// }
