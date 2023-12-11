c10d95ed7262cc2eb1e0b58a5d93adf2a5ba862b
Rhapsody-Zy <151840465+Rhapsody-Zy@users.noreply.github.com>
2023-12-11 22:39:11 +0800

    Add files via upload

---
+++ C-8-Array/8-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    char arr[] = "abcdef";
+    //printf("%c\n",arr[3]);
+    int i = 0;
+    for (i = 0;i<6;i++)
+    {
+        printf("%c ",arr[i]);
+    }
+    return 0;
+}
