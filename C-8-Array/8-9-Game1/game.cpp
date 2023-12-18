32fcca6615440fec8dd7a7ae6450eaf17d322847
Rhapsody-Zy <151840465+Rhapsody-Zy@users.noreply.github.com>
2023-12-18 22:54:28 +0800

    Update game.cpp

--- C-8-Array/8-9-Game1/game.cpp
+++ C-8-Array/8-9-Game1/game.cpp
@@ -12,15 +12,44 @@ void InitBoard(char board[ROW][COL], int row, int col)
         }
     }
 }
+// void DisplayBoard(char board[ROW][COL], int row, int col)
+// {
+//     int i = 0;
+//     for (i = 0; i < ROW; i++)
+//     {
+//         //1.打印一行数据
+//         printf(" %c | %c | %c \n",board[i][0], board[i][1], board[i][2]);
+//         //2.打印一行分割符
+//         if (i < ROW - 1)
+//         printf("---|---|---\n");
+//     }
+// }
 void DisplayBoard(char board[ROW][COL], int row, int col)
 {
     int i = 0;
     for (i = 0; i < ROW; i++)
     {
-        //1.打印一行数据
-        printf(" %c | %c | %c \n",board[i][0], board[i][1], board[i][2]);
-        //2.打印一行分割符
-        if (i < ROW - 1)
-        printf("---|---|---\n");
+        int j = 0;
+        for (j=0;j<col;j++)
+        {
+            //1、打印一行数据
+            printf("  %c", board[i][j]);
+            if (j < col - 1)
+            {
+                printf("|");
+            }     
+        }
+        printf("\n"); 
+            //2、打印分割行
+        if (i < row - 1)
+        {
+            for (j=0;j<col;j++)
+            {
+                printf("---");
+                if (j < col - 1)
+                    printf("|");
+            }
+        }
+        printf("\n");
     }
 }
