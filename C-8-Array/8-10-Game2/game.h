64feb1c5a0893e0c58320afa3aaed13c84297f49
Rhapsody-Zy <151840465+Rhapsody-Zy@users.noreply.github.com>
2023-12-23 23:03:51 +0800

    Update game.h

--- C-8-Array/8-10-Game2/game.h
+++ C-8-Array/8-10-Game2/game.h
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 #define ROW 9
 #define COL 9
+#define Easy_Count 10
 
 #define ROWS ROW+2
 #define COLS COL+2
 
-void InitBoard(char mine[ROWS][COLS], int rows, int cols, char set);
-void DisPlayBoard(char mine[ROWS][COLS], int row, int col);
+
+void InitBoard(char board[ROWS][COLS], int rows, int cols, char set);
+void DisPlayBoard(char board[ROWS][COLS], int row, int col);
+void SetMine(char board[ROWS][COLS], int row, int col);
+void FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col);
